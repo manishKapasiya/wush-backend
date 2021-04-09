2c62cdca816d0893d5750592ca1cff0a27cc1e30
Manish Kapasiya <kapasiya@adobe.com>
2021-04-09 18:23:47 +0530

    Initial commit

---
+++ src/main/java/com/wush/WushBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WushBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WushBackendApplication.class, args);
+	}
+
+}
