dd6093869dd6414255f9f01b3a2f4c6392a9395e
manishKapasiya <manishkapasiya048@gmail.com>
2022-08-17 16:41:44 +0530

    Update WushBackendApplicationTests.java

--- src/test/java/com/wush/WushBackendApplicationTests.java
+++ src/test/java/com/wush/WushBackendApplicationTests.java
@@ -3,6 +3,8 @@ package com.wush;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Hello
+
 @SpringBootTest
 class WushBackendApplicationTests {
 
